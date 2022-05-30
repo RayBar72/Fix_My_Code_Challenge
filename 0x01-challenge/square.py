de713c98f075394906d140a3eccf4b3ad7c00061
RayBar72 <3963@holbertonschool.com>
2022-05-30 16:33:23 -0300

    Task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,6 +8,7 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """Init fuction"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,9 +17,11 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Perimeter function"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """New string function"""
         return "{}/{}".format(self.width, self.height)
 
 
